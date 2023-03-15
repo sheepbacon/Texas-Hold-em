995bcb2168aa8122bc324e20ea216710b36ef8ef
Sheepbacon <72983721+sheepbacon@users.noreply.github.com>
2023-03-16 05:32:56 +0800

    Add files via upload

--- Card.py
+++ Card.py
@@ -1,16 +1,33 @@
 import numpy as np
 
-number = [ "A" , "2" , "3" , "4" , "5" , "6" , "7" , "8", "9" , "10" , "J" , "Q", "K" ]
-color = [ "Spade♠", "Heart♥" , "Diamond♦", "Club♣" ]
+number = [ "2" , "3" , "4" , "5" , "6" , "7" , "8", "9" , "10" , "J" , "Q", "K" , "A" ]
+color = [ " Spade♠", " Heart♥" , " Diamond♦", " Club♣" ]
+number_value = range (13)
+color_order = range (4)
 
 card_set = []
 
+# card information
+class card:
+    
+    def __init__(self, number, number_value, color, color_order, card_number):
+        self.number = number
+        self.color = color
+        self.number_value = number_value
+        self.color_order = color_order
+        self.card_number = card_number
+        
+    def __str__ (self):
+        return self.number +  self.color
+
+# create card set
+num = 0
 for n in range(13):
     for c in range(4):
-        card_set.append (number[n])
-        card_set.append (color[c])
+        cd = card(number[n],number_value[n], color[c], color_order[c], num)
+        card_set.append (cd)
+        num = num+1
 
-card_set = np.reshape(card_set, (52,2))
 
 class cardset:
     
@@ -20,11 +37,14 @@ class cardset:
     def draw(self, d):
         return card_set[d]
     
-    def totaldeck(self):
+    def gettotaldeck(self):
         return card_set
     
 if __name__ == '__main__':
-    pick = cardset()
-    print(card_set[0])
-    print(card_set[0][0])
-    print(card_set[0][1])
+    
+    gg = cardset()
+    lista = gg.gettotaldeck()
+    print(lista[0])
+    print(lista[0].number_value)
+    print(lista[0].number_value*lista[0].number_value)
+    print(lista[0].card_number)
